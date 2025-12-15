6ac9b0fc8ef0d7be054b3d381ac01af3e595f34d
yh <yhoka@naver.com>
2025-12-15 19:47:20 +0900

    12/15 EKS IAM ROLE, Set Worker Node" "

--- yh-ticketing-infra/modules/04-eks/main.tf
+++ yh-ticketing-infra/modules/04-eks/main.tf
@@ -54,3 +54,80 @@ resource "aws_eks_cluster" "ticketing_cluster" {
   }
 }
 
+# EKS Node Group용 IAM Role (Worker Nodes)
+# 실제 워크로드(티켓팅 API Pod)가 실행되는 Worker Node에 AWS 리소스를 사용할 수 있는 신분증을 부여
+# EC2에 있는 Worker Node가 다른 AWS 서비스에 접근하기 위한 IAM Role
+resource "aws_iam_role" "eks_node_role" {
+  name = "ticketing-eks-node-role"
+  asuume_role_policy = jsoncode({ 
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole" # 사용자가 현재 가지고 있는 권한 외에, 다른 IAM 역할(Role)의 권한을 일시적으로 빌려와 사용, EC2가 특정 리소스에 접근할 때 사용(Principal)
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# EKS Workder Node 필수 정책 연결
+# Worker Node가 EKS 컨트롤 플레인에 등록하고, Kubelet과 통신하는 등 쿠버네티스 노드로서 작동하는 데 필요한 가장 기본적인 권한임 (eks_workerNodePolicy)
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# 노드가 VPC CNI를 실행하고 Pod에 VPC IP를 할당하기 위해 VPC 자원(ENI 등)을 관리할 수 있는 권한 (EKS_CNI_Policy)
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# EC2가 ReadOnly로 ECR 리소스에 이미지를 가져올 수 있는 권한을 설정해줌
+# 노드에서 실행되는 Pod가 ECR에 저장된 티켓팅 API Docker 이미지를 읽고 Pull로 다운로드를 할 수 있게 해주는 권한임 (push 권한은 Jenkins한테만 부여)
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly" # ECR에서 이미지 pull 권한
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# EKS Managed Node Group (Worker Nodes)
+resource "aws_eks_node_group" "private_node_group" {
+  cluster_name    = aws_eks_cluster.ticketing_cluster.name
+  node_group_name = "ticketing-private-nodes"
+  subnet_ids      = var.private_subnet_ids # 노드 그룹도 Private Subnet에 배치
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  instance_types  = ["t3.micro"]
+
+  scaling_config {
+    desired_size = 2 # 시작 노드 2개
+    max_size     = 4
+    min_size     = 2
+  }
+
+  # EKS 클러스터가 완전히 생성된 후에 노드 그룹이 생성되도록 명시적 의존성 설정
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_iam_role_policy_attachment.eks_vpc_cni_policy,
+  ]
+
+  tags = {
+    Name = "Ticketing-EKS-Worker_Node"
+  }
+}
+
+# EKS 설정 파일 접속에 필요한 정보를 output으로 내보냄
+output "cluster_name" {
+  value = aws_eks_cluster.ticketing_cluster.name
+}
+
+output "eks_kubeconfig_command" {
+  value       = "aws eks update-kubeconfig --name ${aws_eks_cluster.ticketing_cluster.name} --region ${var.aws_region}"
+  description = "EKS 클러스터에 kubectl로 접속하기 위한 명령어"
+}
+
+output "node_group_name" {
+    value = aws_eks_node_group.private_node_group.node_group_name
+}
